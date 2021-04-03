e957cadbc7aad5316cac3d46b975834d45b7669e
david9morais <david_morais@hotmail.com>
2021-04-03 23:51:36 +0100

    generate class component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
-import './App.css';
-import TodoItem from './components/TodoItem'
-import todoData from './todoData'
+import React from "react";
+import "./App.css";
+import TodoItem from "./components/TodoItem";
+import todoData from "./todoData";
 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todo: todoData,
+    };
+  }
 
-function App() {
+  render() {
+    const todoItems = this.state.todo.map((item) => (
+      <TodoItem key={item.id} item={item} />
+    ));
 
-  const todoItems = todoData.map(item => <TodoItem key={item.id} item={item} />)
-
-  return (
-    <div className="todo-list">
-      {todoItems}
-    </div>
-  );
+    return <div className="todo-list">{todoItems}</div>;
+  }
 }
 
 export default App;
